f9bbddfffa16f75083ec7f34a656f9c6a7b4fabe
DarkNickli <darknickli@gmx.net>
2026-08-17 10:15:38 +0200

    Add files via upload

---
+++ CSSHTscript.js
@@ -0,0 +1,18 @@
+
+<!-- SCRIPT: Kopier den Bereich -->
+<script>
+function copyBereich(button) {
+  const codeBlock = button.closest(".code-block").querySelector(".code");
+  const text = codeBlock.innerText;
+
+  navigator.clipboard.writeText(text).then(() => {
+    const original = button.innerHTML;
+    button.innerHTML = '<span class="check">✓</span>';
+    button.disabled = true;
+
+    setTimeout(() => {
+      button.innerHTML = original;
+      button.disabled = false;
+    }, 1500);
+  });
+}
